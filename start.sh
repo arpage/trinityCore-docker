108156dbbf8cd10ed543d042239bc0de6e7cfe7c
Mark Vitek <straypacket@gmail.com>
2021-11-13 12:56:54 -0500

    latest

--- start.sh
+++ start.sh
@@ -2,7 +2,6 @@
 
 export BASEHTML="/srv/wow/trinitycore/3.3.5a"
 export DOCROOT="/srv/wow/trinitycore/3.3.5a"
-export GRPID=1000
 export LOCAL_IP=$(hostname -I | awk '{print $1}')
 export HOSTIP=$(/sbin/ip route | awk '/default/ { print $3 }')
 
@@ -19,70 +18,26 @@ echo "${HOSTIP} dockerhost" >>/etc/hosts
 # 7. run sql import files.
 #
 
-
-
-# Setup Drupal if services.yml or settings.php is missing
-if (! grep -q 'database.*=>.*drupal' ${DOCROOT}/sites/default/settings.php 2>/dev/null); then
-  # Generate random passwords
-  DRUPAL_DB="drupal"
-  DBPASS=$(grep password /etc/mysql/debian.cnf | head -n1 | awk '{print $3}')
-  ROOT_PASSWORD=$(pwgen -c -n -1 12)
-  DRUPAL_PASSWORD=$(pwgen -c -n -1 12)
-  echo ${ROOT_PASSWORD} >/var/lib/mysql/mysql/mysql-root-pw.txt
-  echo ${DRUPAL_PASSWORD} >/var/lib/mysql/mysql/drupal-db-pw.txt
-  # Wait for mysql
-  echo -n "Waiting for mysql "
-  while ! mysqladmin status >/dev/null 2>&1; do
-    echo -n .
-    sleep 1
+#if [ ! -f /var/trinityscripts/create_mysql.sql ]; then
+if [ z != z ]; then
+  cp /srv/wow/trinitycore/3.3.5a/sql/create/create_mysql.sql /var/trinityscripts
+  TRINITY_PASSWORD=$(cat /var/trinityscripts/tpwd)
+  sed -i "s/ED BY 'trinity'/ED BY '${TRINITY_PASSWORD}'/" /var/trinityscripts/create_mysql.sql
+  ROOT_PASSWORD=$(cat /var/trinityscripts/rpwd)
+  cat /var/trinityscripts/create_mysql.sql | mysql -u root -p${ROOT_PASSWORD} -h trinity-db
+  for f in local-sql/20*.sql; do
+    bn=$(basename $f .sql)
+    schema=$(echo $bn | cut -d '.' -f 2)
+    echo "${f} > ${bn} > ${schema}"
+    echo "cat $f | mysql -u trinity -p${TRINITY_PASSWORD} ${schema}"
   done
-  echo
-  # Create and change MySQL creds
-  mysqladmin -u root password ${ROOT_PASSWORD} 2>/dev/null
-  echo -e "[client]\npassword=${ROOT_PASSWORD}\n" >/root/.my.cnf
-  mysql -e \
-    "CREATE USER 'debian-sys-maint'@'localhost' IDENTIFIED WITH mysql_native_password BY '${DBPASS}';
-         GRANT ALL ON *.* TO 'debian-sys-maint'@'localhost';
-         CREATE DATABASE ${DRUPAL_DB};
-         CREATE USER 'drupal'@'%' IDENTIFIED WITH mysql_native_password BY '${DRUPAL_PASSWORD}';
-         GRANT ALL ON ${DRUPAL_DB}.* TO 'drupal'@'%';
-         FLUSH PRIVILEGES;"
-else
-  echo "**** ${DOCROOT}/sites/default/settings.php database found ****"
-  ROOT_PASSWORD=$(cat /var/lib/mysql/mysql/mysql-root-pw.txt)
-  DRUPAL_PASSWORD=$(cat /var/lib/mysql/mysql/drupal-db-pw.txt)
 fi
-
-# Change root password
-echo "root:${ROOT_PASSWORD}" | chpasswd
-
-# Clear caches and reset files perms
-XDEBUG_LOG=/tmp/xdebug.log
-XDEBUG_HOST=`netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}'`
-sed -i "s/XDEBUG_HOST/$XDEBUG_HOST/" /etc/php/7.4/mods-available/xdebug.ini
-touch $XDEBUG_LOG
-chown www-data:${GRPID} $XDEBUG_LOG
-chmod ugo+rw $XDEBUG_LOG
-chown -R www-data:${GRPID} ${DOCROOT}/sites/default/
-chmod -R ug+w ${DOCROOT}/sites/default/
-chown -R mysql:${GRPID} /var/lib/mysql/
-chmod -R ug+w /var/lib/mysql/
-find -type d -exec chmod +xr {} \;
-(
-  sleep 3
-  drush --root=${DOCROOT}/ cache-rebuild 2>/dev/null
-) &
 
 echo
 echo "---------------------- USERS CREDENTIALS ($(date +%T)) -------------------------------"
 echo
-echo "    DRUPAL:  http://${LOCAL_IP}              with user/pass: admin/admin"
-echo
-echo "    MYSQL :  http://${LOCAL_IP}/adminer.php  drupal/${DRUPAL_PASSWORD} or root/${ROOT_PASSWORD}"
-echo "    SSH   :  ssh root@${LOCAL_IP}            with user/pass: root/${ROOT_PASSWORD}"
-echo
-echo "  Please report any issues to https://github.com/ricardoamaro/drupal9-docker-app"
-echo "  USE CTRL+C TO STOP THIS APP"
+echo "    ${LOCAL_IP}              with user/pass: trinity/${TRINITY_PASSWORD}"
+echo "    ${LOCAL_IP}              with user/pass: root/${ROOT_PASSWORD}"
 echo
 echo "------------------------------ STARTING SERVICES ---------------------------------------"
 
