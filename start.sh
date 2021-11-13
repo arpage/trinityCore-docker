f78bd017bf55a8108bdfd28dd09b5b1c9b872b6d
Mark Vitek <straypacket@gmail.com>
2021-11-13 11:27:52 -0500

    latest

--- start.sh
+++ start.sh
@@ -7,21 +7,19 @@ export LOCAL_IP=$(hostname -I | awk '{print $1}')
 export HOSTIP=$(/sbin/ip route | awk '/default/ { print $3 }')
 
 echo "${HOSTIP} dockerhost" >>/etc/hosts
-echo "Started Container: $(date)"
 
-# Create a basic mysql install
-if [ ! -d /var/lib/mysql/mysql ]; then
-  echo "**** No MySQL data found. Creating data on /var/lib/mysql/ ****"
-  rm -rf /var/lib/mysql/*;
-  /usr/sbin/mysqld --initialize-insecure
-  sed -i 's/^bind-address/#bind-address/g' /etc/mysql/mysql.conf.d/mysqld.cnf;
-  sed -i "s/Basic Settings/Basic Settings\ndefault-authentication-plugin=mysql_native_password\n/" /etc/mysql/mysql.conf.d/mysqld.cnf
-else
-  echo "**** MySQL data found on /var/lib/mysql/ ****"
-fi
+#
+# 0. copy sql create file.
+# 1. copy sql import files
+# 2. create trinity passwd.
+# 3. sed passwd in sql create file.
+# 4. sed passwd in worldserver.conf.
+# 5. sed passwd in authserver.conf.
+# 6. run sql create file.
+# 7. run sql import files.
+#
 
-# Start supervisord
-#supervisord -c /etc/supervisor/conf.d/supervisord.conf -l /tmp/supervisord.log
+
 
 # Setup Drupal if services.yml or settings.php is missing
 if (! grep -q 'database.*=>.*drupal' ${DOCROOT}/sites/default/settings.php 2>/dev/null); then
@@ -45,23 +43,10 @@ if (! grep -q 'database.*=>.*drupal' ${DOCROOT}/sites/default/settings.php 2>/de
   mysql -e \
     "CREATE USER 'debian-sys-maint'@'localhost' IDENTIFIED WITH mysql_native_password BY '${DBPASS}';
          GRANT ALL ON *.* TO 'debian-sys-maint'@'localhost';
-         CREATE DATABASE drupal;
+         CREATE DATABASE ${DRUPAL_DB};
          CREATE USER 'drupal'@'%' IDENTIFIED WITH mysql_native_password BY '${DRUPAL_PASSWORD}';
-         GRANT ALL ON drupal.* TO 'drupal'@'%';
+         GRANT ALL ON ${DRUPAL_DB}.* TO 'drupal'@'%';
          FLUSH PRIVILEGES;"
-  cd ${DOCROOT}
-  cp sites/default/default.settings.php sites/default/settings.php
-  cp sites/example.settings.local.php sites/default/settings.local.php
-  ${DRUSH} site-install standard -y --account-name=admin --account-pass=admin \
-    --db-url="mysql://drupal:${DRUPAL_PASSWORD}@localhost:3306/drupal" \
-    --site-name="CAP Testing Services" | grep -v 'continue?' 2>/dev/null
-  ${DRUSH} cr
-  ${DRUSH} cset system.site uuid "6abb4f05-743c-4ed5-a90a-64c5fd121bca" -y
-  ${DRUSH} scr clear_shortcut_set.php
-  ${DRUSH} cim -y
-  ${DRUSH} cr
-  ${DRUSH} tome:import -y
-  ${DRUSH} user:password admin "admin"
 else
   echo "**** ${DOCROOT}/sites/default/settings.php database found ****"
   ROOT_PASSWORD=$(cat /var/lib/mysql/mysql/mysql-root-pw.txt)
