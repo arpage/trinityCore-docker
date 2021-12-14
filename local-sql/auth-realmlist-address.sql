a746fb247f2c69421206ad92f69f3ca8084c042f
Ubuntu <ubuntu@ip-172-31-18-248.us-east-2.compute.internal>
2021-12-14 07:39:16 -0500

    local changes

--- local-sql/auth-realmlist-address.sql
+++ local-sql/auth-realmlist-address.sql
@@ -1,4 +1,4 @@
 ###
-update realmlist set address="192.168.1.137" where id = 1;
+update realmlist set address="pk.straypacket.com" where id = 1;
 
 
--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -5,6 +5,10 @@
 iptables -A INPUT -p tcp --dport 3724 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp --sport 3724 -m state --state ESTABLISHED -j ACCEPT
 
-# server 1
+#  World Server
 iptables -A INPUT -p tcp --dport 8085 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp --sport 8085 -m state --state ESTABLISHED -j ACCEPT
+
+# World Server - SOAP
+iptables -A INPUT -p tcp --dport 7878 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 7878 -m state --state ESTABLISHED -j ACCEPT
